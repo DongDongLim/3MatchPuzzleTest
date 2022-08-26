b43474f51b2dceceab77bdcf5d76ae3567a5e8b4
Lim DongHyen <99638155+DongDongLim@users.noreply.github.com>
2022-08-26 10:08:00 +0900

    refactor : PuzzleNodes.m_NodeIndexs -> SharedData.m_NodeIndexs

--- Assets/Script/PuzzleNodes.cs
+++ Assets/Script/PuzzleNodes.cs
@@ -6,16 +6,14 @@ public class PuzzleNodes : MonoBehaviour
 {
     private int m_MaxWidth;
     private int m_MaxHeight;
-    public int[,] m_NodeIndexs;
     private void Awake()
     {
         m_MaxWidth = SharedData.instance.MaxWidth;
         m_MaxHeight = SharedData.instance.MaxHight;
-        m_NodeIndexs = new int[m_MaxHeight, m_MaxWidth];
         for (int i = 0; i < m_MaxHeight; ++i)
         {
             for (int j = 0; j < m_MaxWidth; ++j)
-                m_NodeIndexs[i, j] = (i * m_MaxWidth) + j;
+                SharedData.instance.SetNodeIndexs(i, j, (i * m_MaxWidth) + j);
         }
     }
 
--- Assets/Script/SharedData.cs
+++ Assets/Script/SharedData.cs
@@ -11,14 +11,27 @@ public class SharedData : SingleTonOnly<SharedData>
 
     private int m_MaxPoolCount;
 
+    private int[,] m_NodeIndexs;
+
     public int MaxWidth { get { return m_MaxWidth; } }
 
     public int MaxHight { get { return m_MaxHeight; } }
 
     public int MaxPoolCount { get { return m_MaxPoolCount; } }
 
+    public void SetNodeIndexs(int height, int width, int value)
+    {
+        m_NodeIndexs[height, width] = value;
+    }
+
+    public int GetNodeIndexs(int height, int width)
+    {
+        return m_NodeIndexs[height, width];
+    }
+
     protected override void OnAwake()
     {
+        m_NodeIndexs = new int[m_MaxHeight, m_MaxWidth];
         m_MaxPoolCount = m_MaxWidth * m_MaxHeight;
     }
 }
